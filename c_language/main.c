0fb3919c10fd9682988c3a6a9965b5c1299d9e84
surajkumar2026 <suraj9798415340@gmail.com>
2024-10-15 23:56:19 +0530

    Add files via upload
    this is my first file upload.

---
+++ c_language/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+  int age = 25;
+  double cgpa = 9.9;
+  char name  []= "Karthi";
+  float size = 3.55;
+  printf("%d %s %f %lf ",age,name,size,cgpa);
+
+   return 0;
+
+}
